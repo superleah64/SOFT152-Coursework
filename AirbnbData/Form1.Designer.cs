b366235aee7b4b1dd4c85f7095cd7cc31cc19f54
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2019-12-04 11:02:25 +0000

    Updated form interface
    Added in 'show data' & 'edit data' buttons, added a logo & added some comments

--- AirbnbData/Form1.Designer.cs
+++ AirbnbData/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.lstNeighbourhoods = new System.Windows.Forms.ListBox();
             this.lstProperties = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lstDistricts
@@ -62,23 +66,57 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(369, 60);
+            this.label1.Location = new System.Drawing.Point(331, 89);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 24);
+            this.label1.Size = new System.Drawing.Size(200, 24);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Airbnb Data";
+            this.label1.Text = "Airbnb Data Viewer";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(64, 89);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(177, 45);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Show Data";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.BtnDist_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(623, 89);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(177, 45);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit Data";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AirbnbData.Properties.Resources.AirbnbLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(335, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 460);
+            this.ClientSize = new System.Drawing.Size(860, 455);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnShow);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lstProperties);
             this.Controls.Add(this.lstNeighbourhoods);
             this.Controls.Add(this.lstDistricts);
             this.Name = "Form1";
-            this.Text = "Airbnb Data";
+            this.Text = "Airbnb Data Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +128,9 @@
         private System.Windows.Forms.ListBox lstNeighbourhoods;
         private System.Windows.Forms.ListBox lstProperties;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- AirbnbData/Form1.cs
+++ AirbnbData/Form1.cs
@@ -16,5 +16,17 @@ namespace AirbnbData
         {
             InitializeComponent();
         }
+
+        private void BtnDist_Click(object sender, EventArgs e)
+        {
+            // this will show the districts in the list box
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            // this will allow admin to edit the selected piece of data
+        }
     }
 }
+
+// we want to click a button to show the districts, then click a district to show the neighbourhoods, then click a neighbourhood to show the properties
